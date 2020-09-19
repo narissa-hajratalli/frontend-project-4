f59200da89cc04326425844fb60c2d0dab68a70b
NARISSA <nkh239@nyu.edu>
2020-09-18 21:54:13 -0400

    added input fields

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,34 +24,17 @@
         </b-datepicker>
       </b-field>
 
-        <b-field>
-            <b-input placeholder="Did you consume meat today?"
-                type="number"
-                min="10"
-                max="20">
-            </b-input>
-        </b-field>
+      <b-field label="Ate meat? Be honest; it's okay!">
+        <b-select placeholder="Ate meat? Be honest--it's okay!">
+          <option>Yes</option>
+          <option>No</option>
+        </b-select>
+      </b-field>
 
-        <b-field>
-            <b-input placeholder="User handle (custom validation for only lowercase)"
-              type="text"
-              required
-              validation-message="Only lowercase is allowed"
-              pattern="[a-z]*">
-            </b-input>
-        </b-field>
-
-        <b-field>
-            <b-input placeholder="URL" type="url"></b-input>
-        </b-field>
-
-        <b-field>
-            <b-input type="textarea"
-                minlength="10"
-                maxlength="100"
-                placeholder="Maxlength automatically counts characters">
-            </b-input>
-        </b-field>
+      <b-field label="How many">
+        <b-numberinput type="is-success" min=0></b-numberinput>
+      </b-field>
+            
     </section>
   
 
